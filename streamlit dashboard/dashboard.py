2ad3aefd4e806b2374c0daa3b07c57624d210942
Julia Gastellu <juliacgastellu@gmail.com>
2024-10-02 08:58:59 -0300

    dasboard en proceso

--- streamlit dashboard/dashboard.py
+++ streamlit dashboard/dashboard.py
@@ -18,6 +18,43 @@ Provincias = sorted(list(mapa_conectividad['Provincia'].unique()))
 # Crear dos columnas
 col1, col2 = st.columns([1, 1])  # La primera columna ocupa 3 unidades y la segunda 1 
 
+
+def crear_grafico(df):
+    # Agrupar y calcular estadísticas
+    df_mapa = mapa_conectividad.groupby('Provincia').agg({
+        'Población': 'sum',
+        'Latitud': 'mean',
+        'Longitud': 'mean',
+      
+    }).reset_index()
+
+    # Crear el mapa
+    fig = px.scatter_geo(df_mapa,
+                        lat='Latitud',
+                        lon='Longitud',
+                        scope='south america',
+                        color='Población',  # Color según el porcentaje conectado
+                        color_continuous_scale='Viridis',  # Escala de colores
+                        size='Población',
+                        hover_name='Provincia',
+                        hover_data=['Población'],
+                        title='Conectividad a Internet por Provincia',
+                        template='plotly_dark'  # Mapa base oscuro
+                       )
+
+    # Personalizar el mapa
+    fig.update_layout(
+        geo=dict(
+            showcountries=True,
+            countrycolor="LightPink",
+            showframe=False,
+            showcoastlines=False,
+            projection_type="orthographic"
+        )
+    )
+
+    return fig
+
 # En la primera columna, mostrar el mapa
 with col1: 
     
